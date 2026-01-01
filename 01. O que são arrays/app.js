0cf595f9b835b603c5d2712508bb2fa209454654
mininoterraria <diegocesarrufino@outlook.com.br>
2026-01-01 11:11:20 -0300

    Prática

--- 01. O que são arrays/app.js
+++ 01. O que são arrays/app.js
@@ -1,3 +1,5 @@
+//AVISO: ELIMINAR A CADEIA DE COMENTÁRIO DE CADA QUESTÃO SE QUISER RODAR CADA CÓDIGO.
+
 //01. Um array esparso é um tipo de array no qual a maioria dos elementos tem valores indefinidos. Crie um array esparso e atribua valores a alguns elementos do array (por exemplo, defina valores nos índices 1, 3 e 7). Imprima no console o array e também a informação de comprimento do array.
 
 /*
