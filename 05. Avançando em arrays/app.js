affd9754cf346af0ada137587689145ef56a0899
mininoterraria <diegocesarrufino@outlook.com.br>
2026-01-03 11:36:43 -0300

    Prática

--- 05. Avançando em arrays/app.js
+++ 05. Avançando em arrays/app.js
@@ -1,3 +1,5 @@
+//AVISO: ELIMINAR A CADEIA DE COMENTÁRIO DE CADA QUESTÃO SE QUISER RODAR CADA CÓDIGO.
+
 //01. Faça uma função que aceite vários arrays como argumentos e retorne um único array contendo todos os elementos dos arrays fornecidos, utilizando Spread Operator.
 
 /*
